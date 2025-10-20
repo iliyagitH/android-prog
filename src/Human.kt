64f073b6f678339d3cbb68e8244e4665a6399f77
Ilya <lutsenko5400@list.ru>
2025-10-21 01:30:30 +0700

    Lab is complete

--- src/Human.kt
+++ src/Human.kt
@@ -15,15 +15,4 @@ class Human(
         y += deltaY
         println("$fullName переместился в точку (${"%.2f".format(x)}, ${"%.2f".format(y)})")
     }
-
-    fun moveGaussMarkov(previousDirection: Double, alpha: Double = 0.8): Double {
-        val randomDeviation = (Math.random() - 0.5) * Math.PI / 4
-        val newDirection = alpha * previousDirection + (1 - alpha) * randomDeviation
-        val normalizedDirection = (newDirection + 2 * Math.PI) % (2 * Math.PI)
-        val stepLength = currentSpeed * 0.8 + Math.random() * currentSpeed * 0.4
-        x += stepLength * kotlin.math.cos(normalizedDirection)
-        y += stepLength * kotlin.math.sin(normalizedDirection)
-        println("$fullName (Gauss-Markov) -> (${"%.2f".format(x)}, ${"%.2f".format(y)})")
-        return normalizedDirection
-    }
 }
